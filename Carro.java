3c0ff2b5d3b7ef1dc90b4f0a0e749b6b1872ad5b
DuduXXXX <132277531+DuduXXXX@users.noreply.github.com>
2024-05-21 21:16:31 -0300

    Add files via upload

---
+++ Carro.java
@@ -0,0 +1,78 @@
+public class Carro {
+    private String tipo;
+    private String marca;
+    private String modelo;
+    private int ano;
+    private int numeroPortas;
+    private String placa;
+
+
+    public Carro(String tipo, String marca, String modelo, int ano, int numeroPortas , String placa) {
+        this.tipo = tipo;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.numeroPortas = numeroPortas;
+        this.placa = placa;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getNumeroPortas() {
+        return numeroPortas;
+    }
+
+    public void setNumeroPortas(int numeroPortas) {
+        this.numeroPortas = numeroPortas;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String toString() {
+        return 
+        " - Tipo: " + tipo +
+        "\n - Marca: " + marca + 
+        "\n - Modelo: " + modelo + 
+        "\n - Ano: " + ano + 
+        "\n - Numero de Portas: " + numeroPortas +
+        "\n - Placa: " + placa + 
+        "\n";
+    }
+
+}
---
+++ Utilidades.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utilidades {
+    private final List<Carro> lista;
+
+    public Utilidades() {
+        lista = new ArrayList<>();
+    }
+
+    public List<Carro> getList(){
+        return lista;
+    }
+
+    // ADICIONAR VEICULO
+    public void adicionarVeiculo(Carro carro) throws Exception {
+        if (carro.getTipo() == null || carro.getTipo().isEmpty()){
+            throw new Exception("O tipo do veículo não pode ser nulo.");
+        }
+        if (carro.getMarca() == null || carro.getMarca().isEmpty()) {
+            throw new Exception("A marca do veículo não pode ser nulo.");
+        }
+        if(carro.getModelo() == null || carro.getModelo().isEmpty()) { 
+            throw new Exception("O modelo do veículo não pode ser nulo.");
+        }
+        if(carro.getAno() < 1885) { 
+            throw new Exception("O ano deve ser maior que 0.");
+        }
+        if(carro.getNumeroPortas() < 1 && carro.getTipo().equalsIgnoreCase("carro") ) {
+            throw new Exception("O carro deve ter um número de portas maior que 0.");
+        }
+        if(carro.getPlaca() == null || carro.getPlaca().isEmpty()) { 
+            throw new Exception("A placa é obrigatória.");
+        }
+        for (Carro L : lista){
+            if(L.getPlaca().equalsIgnoreCase(carro.getPlaca())) {
+                throw new Exception("Já existe um carro com essa placa cadastrado.");
+            }
+        }
+
+        lista.add(carro);
+
+    }
+
+    // REMOVER
+    public void removerVeiculo(Carro carro) {
+        lista.remove(carro);
+    }
+
+    // LISTAR OS VEÍCULOS
+    public void listaVeiculo() {
+        if(lista.isEmpty()){
+            System.out.println("Não há veículos para serem listados.");
+            return;
+        }
+        System.out.println("=== Veículos na Lista ===");
+        for (Carro carro : lista){
+            System.out.println(carro);
+        }
+    }
+
+    // BUSCAR
+    public Carro buscarCarro(String placa){
+        for(Carro carro : lista){
+            if(carro.getPlaca().equalsIgnoreCase(placa)){
+                return carro;
+            }
+        }
+        return null;
+    }
+}
